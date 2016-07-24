991f18ca41860c1938c29dd4f7e3e5eb58c81b1d
egusa hiroaki <egusa.hiroaki@gmail.com>
2016-07-24 22:42:02 +0900

    [proto]

--- README.md
+++ README.md
@@ -18,7 +18,20 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+引数の型は配列。
+
+[A B, C] => (A and B) or C
+[A B, C D, E] => (A adn B) or (C and D) or E
+
+andor = AndOr.new("test1 test2, test3")
+andor.classify
+target_content = "This is test1."
+andor.check_single(target_content)
+andor.check_multi(target_content)
+andor.result
+
+
+
 
 ## Contributing
 
--- lib/and_or.rb
+++ lib/and_or.rb
@@ -1,5 +1,67 @@
 require "and_or/version"
 
 module AndOr
-  # Your code goes here...
+  class RegExp
+    if value.kind_of? Array
+      @regexp = value.split(",").dup
+    else
+      @regexp = Array(value.split(",").dup)
+    end
+
+    # 合致しているかどうかのフラグ
+    # true:  条件を満たす
+    # false: 条件を満たさない
+    @match = false
+  end
+
+  # 単一入力と複数入力のものを分類する
+  def classify
+    @multi_elms  = []
+    @single_elms = []
+    @regexp.each do |regexp|
+      if regexp.split(" ").length > 1 # 複数語が入寮されている時
+        @multi_elms << regexp
+      else
+        @single_elms << regexp
+      end
+    end
+  end
+
+  #[TODO] より良い書き方
+  def check_single(content)
+    if @single_elms.length > 0
+      @single_elms.each do |single_elm|
+        deleted_space_elm = single_elm.strip
+        @match =  true  if content.include?  deleted_space_elm
+      end
+    end
+  end
+
+  # [TODO] より良い書き方
+  def check_multi(content)
+    # 条件一致がない場合のみ、ここの処理は走らせる（効率化のため）
+    if @multi_elms.length > 0 && !@match
+      @multi_elms.each do |multi_elm|
+        # ここの処理でスペースを削除し、文字列の一致を見る。
+
+        # 単語の数を初期化
+        and_num = 0
+
+        # スペースで分割、空文字、nilを削除する
+        target_words = multi_elm.split(/[[:space:]]/).compact.reject(&:empty?)
+        target_words.each do |word|
+          and_num+=1 if content.include? word
+        end
+
+        if and_num == target_words.length
+          @match = true
+        end
+      end
+    end
+  end
+
+  # 入力に対する結果
+  def result
+    @match
+  end
 end
