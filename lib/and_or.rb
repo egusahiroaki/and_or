81a3d67159dbce0545205cc0697bd1b92b8917a2
egusa hiroaki <egusa.hiroaki@gmail.com>
2016-07-24 22:58:41 +0900

    fix

--- lib/and_or.rb
+++ lib/and_or.rb
@@ -8,18 +8,17 @@ module AndOr
       @regexp = Array(value.split(",").dup)
     end
 
-    # 合致しているかどうかのフラグ
-    # true:  条件を満たす
-    # false: 条件を満たさない
+    # flag for content match
+    # true:  match
+    # false: not match
     @match = false
   end
 
-  # 単一入力と複数入力のものを分類する
   def classify
     @multi_elms  = []
     @single_elms = []
     @regexp.each do |regexp|
-      if regexp.split(" ").length > 1 # 複数語が入寮されている時
+      if regexp.split(" ").length > 1 # if arguments are multi words
         @multi_elms << regexp
       else
         @single_elms << regexp
@@ -27,7 +26,6 @@ module AndOr
     end
   end
 
-  #[TODO] より良い書き方
   def check_single(content)
     if @single_elms.length > 0
       @single_elms.each do |single_elm|
@@ -37,17 +35,10 @@ module AndOr
     end
   end
 
-  # [TODO] より良い書き方
   def check_multi(content)
-    # 条件一致がない場合のみ、ここの処理は走らせる（効率化のため）
     if @multi_elms.length > 0 && !@match
       @multi_elms.each do |multi_elm|
-        # ここの処理でスペースを削除し、文字列の一致を見る。
-
-        # 単語の数を初期化
         and_num = 0
-
-        # スペースで分割、空文字、nilを削除する
         target_words = multi_elm.split(/[[:space:]]/).compact.reject(&:empty?)
         target_words.each do |word|
           and_num+=1 if content.include? word
@@ -60,7 +51,6 @@ module AndOr
     end
   end
 
-  # 入力に対する結果
   def result
     @match
   end
